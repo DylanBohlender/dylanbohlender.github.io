6fd290c1a8f21f76290b19980792e383a27846ca
Dylan Bohlender <dylan.bohlender@gmail.com>
2018-07-01 01:26:51 -0600

    Website first draft

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var wait = require('gulp-wait');
+
+gulp.task('styles', function () {
+    return gulp.src('./scss/styles.scss')
+        .pipe(wait(250))
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('watch', ['styles'], function() {
+    gulp.watch('scss/*.scss', ['styles']);
+});
